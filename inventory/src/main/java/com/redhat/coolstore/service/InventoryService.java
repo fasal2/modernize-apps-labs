b4a4b96d7642e4f18868c13d7604f524d0ada139
Fasal Zaman <fasal.zaman@gmail.com>
2020-03-10 01:12:20 +0530

    Update InventoryService.java

--- inventory/src/main/java/com/redhat/coolstore/service/InventoryService.java
+++ inventory/src/main/java/com/redhat/coolstore/service/InventoryService.java
@@ -1,37 +1,34 @@
 package com.redhat.coolstore.service;
 
-
-import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
 import com.redhat.coolstore.model.Inventory;
-
-import java.util.Collection;
 import java.util.List;
 
-@Stateless
+import javax.inject.Inject;
+import javax.enterprise.context.Dependent;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+@Dependent
 public class InventoryService {
 
-    @PersistenceContext
-    private EntityManager em;
-
+    @Inject
+    EntityManager entityManager;
+   
     public InventoryService() {
 
     }
 
     public boolean isAlive() {
-        return em.createQuery("select 1 from Inventory i")
+        return entityManager.createQuery("select 1 from Inventory i")
                 .setMaxResults(1)
                 .getResultList().size() == 1;
     }
     public Inventory getInventory(String itemId) {
-        return em.find(Inventory.class, itemId);
+        return entityManager.find(Inventory.class, itemId);
     }
 
     public List<Inventory> getAllInventory() {
-        Query query = em.createQuery("SELECT i FROM Inventory i");
+        Query query = entityManager.createQuery("SELECT i FROM Inventory i");
         return query.getResultList();
     }
 }
